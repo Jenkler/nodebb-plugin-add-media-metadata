2f1e4eeaf1f9dbd5dcee8d5bfb01b46e6205dfcd
Mikael Jenkler <mikael@jenkler.com>
2020-06-28 19:35:58 +0200

    Updated LICENSE

--- client.js
+++ client.js
@@ -66,7 +66,7 @@ $(window).on('action:composer.loaded', function (event, data) {
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Film-ID</h5>
+          <h5 class="modal-title" id="exampleModalLabel">Imdb-ID</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
